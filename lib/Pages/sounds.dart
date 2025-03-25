9d53418b9b6fcb2836c238b438772c3eb5b04efd
lee03840 <lee03840@d.umn.edu>
2025-03-25 14:35:10 -0500

    Revert "Added basic SoundWidget"
    This reverts commit b1c83cad2b0c08fabcf09bd175c0eb59930ca502.

--- lib/Pages/sounds.dart
+++ lib/Pages/sounds.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sleep_app/dreams/viewmodel/SoundsModel.dart';
-import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -44,9 +42,7 @@ class _MySoundPageState extends State<MySoundPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SoundWidget(filePath: "Sound/file/path", name: "Sound name"),
-            const Divider(height: 10, thickness: 1, indent: 0, endIndent: 0, color: Colors.white),
-            SoundWidget(filePath: "Sound/file/path", name: "Sound name 2")
+            const Text('Filler'),
           ],
         )
       ),
@@ -54,51 +50,3 @@ class _MySoundPageState extends State<MySoundPage> {
   }
 }
 
-class SoundWidget extends StatefulWidget {
-  final String name;
-  final String filePath;
-
-  const SoundWidget({Key? key, required this.filePath, required this.name}) : super(key: key);
-
-  @override
-  _SoundWidgetState createState() => _SoundWidgetState();
-
-}
-
-class _SoundWidgetState extends State<SoundWidget> {
-  final AudioPlayer _audioPlayer = AudioPlayer();
-
-  void _playSound() async {
-    try {
-      await _audioPlayer.play(AssetSource(widget.filePath));
-    } catch (e) {
-      debugPrint("Error playing sound: $e");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-     onTap: _playSound,
-     child: Container(
-       padding: const EdgeInsets.all(12),
-       decoration: BoxDecoration(
-         color: Colors.deepPurple,
-         borderRadius: BorderRadius.circular(10),
-       ),
-       child: Center(
-         child: Text(
-           widget.name,
-           style: const TextStyle(color: Colors.black, fontSize: 16),
-         ),
-       )
-     ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
-  }
-}
