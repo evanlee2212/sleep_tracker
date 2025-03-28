f1f44ddbc9cd6ae37c6880658344779d501cdf3e
TSchmidt916 <schm5669@d.umn.edu>
2025-03-27 22:36:57 -0500

    Reimplemented Firebase
    Firebase and its dependencies are readded and the app no longer crashes

--- lib/Pages/main.dart
+++ lib/Pages/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sleep_app/Pages/sleep_data.dart';
 import 'package:sleep_app/Pages/sounds.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
