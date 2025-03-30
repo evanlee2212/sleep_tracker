6dd6e333901252f787f0f8db3400612e1567b83c
chock023 <chock023@d.umn.edu>
2025-03-30 17:27:56 -0500

    Initial notification feature structure

--- lib/Pages/main.dart
+++ lib/Pages/main.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:sleep_app/Pages/sleep_data.dart';
 import 'package:sleep_app/Pages/sounds.dart';
 import 'package:sleep_app/Pages/notification.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); // Required for async calls before runApp
+  AwesomeNotifications().initialize(
+    // set the icon to null if you want to use the default app icon
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'scheduled_channel',
+        channelName: 'Scheduled Notifications',
+        channelDescription: 'Notification channel for scheduled notifications',
+        defaultColor: Color(0xFF9D50DD),
+        ledColor: Colors.white,
+        importance: NotificationImportance.High,
+        channelShowBadge: true,
+        locked: true,
+        defaultRingtoneType: DefaultRingtoneType.Notification,
+      ),
+    ],
+  );
   runApp(const MyApp());
 }
 
@@ -30,6 +49,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+
+  void initState() {
+    super.initState(); // Call the superclass's initState() first
+
+    // Request notification permissions
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Allow Notifications'),
+            content: Text(
+                'Our app would like to send you notifications to remind you to sleep and wake up.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Don\'t Allow'),
+              ),
+              TextButton(
+                onPressed: () => AwesomeNotifications()
+                    .requestPermissionToSendNotifications()
+                    .then((_) => Navigator.pop(context)),
+                child: Text('Allow'),
+              ),
+            ],
+          ),
+        );
+      }
+    });
+    _scheduleSleepNotification();
+    _scheduleWakeUpNotification();
+  }
+
+  void _scheduleSleepNotification() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 1, // Unique ID for the notification
+        channelKey: 'scheduled_channel',
+        title: 'Time to Sleep!',
+        body: 'It\'s time to wind down and get ready for bed.',
+        notificationLayout: NotificationLayout.Default,
+      ),
+      schedule: NotificationCalendar(
+        hour: 22, // 10 PM
+        minute: 0,
+        second: 0,
+        repeats: true, // Repeat every day
+      ),
+    );
+  }
+  void _scheduleWakeUpNotification() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 2, // Unique ID for the notification
+        channelKey: 'scheduled_channel',
+        title: 'Good Morning!',
+        body: 'It\'s time to wake up and start your day.',
+        notificationLayout: NotificationLayout.Default,
+      ),
+      schedule: NotificationCalendar(
+        hour: 7, // 7 AM
+        minute: 0,
+        second: 0,
+        repeats: true, // Repeat every day
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import awesome_notifications
 import file_picker
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
