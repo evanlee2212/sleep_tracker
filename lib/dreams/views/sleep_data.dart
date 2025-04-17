6065039b85cd1eb0a0a139e4525ae74dad98d0bc
patta215 <patta215@d.umn.edu>
2025-04-17 00:42:44 -0500

    added screen_time.dart and removed some audio files

--- lib/dreams/views/sleep_data.dart
+++ lib/dreams/views/sleep_data.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sleep_app/Pages/sleepDiary.dart';
 import 'package:sleep_app/Pages/sleep_cycles.dart';
 import 'package:sleep_app/Pages/statistics.dart';
+import 'package:sleep_app/Pages/screen_time.dart';
 import 'package:sleep_app/components/menu_button.dart';
 import 'package:sleep_app/Pages/sleepTracker.dart';
 import 'package:sleep_app/dreams/contracts/sleep_data_contract.dart';
@@ -9,7 +10,7 @@ import 'package:sleep_app/dreams/presenter/sleep_data_presenter.dart';
 import '../viewmodel/sleepDiaryModel.dart';
 
 class SleepData extends StatefulWidget {
-   const SleepData({super.key});
+  const SleepData({super.key});
 
   @override
   State<SleepData> createState() => _SleepDataState();
@@ -28,31 +29,41 @@ class _SleepDataState extends State<SleepData> implements SleepDataContractView 
 
   @override
   void navigateToSleepDiary() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => SleepDiaryPage(diaryModel: _presenter.diaryModel)));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SleepDiaryPage(diaryModel: _presenter.diaryModel)),
+    );
   }
 
   @override
   void navigateToSleepTracker() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => SleepTracker()));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SleepTracker()),
+    );
   }
 
   @override
-  void navigateToSleepCycles(){
-    Navigator.push(context, MaterialPageRoute(builder: (context) => SleepCycles()));
+  void navigateToSleepCycles() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SleepCycles()),
+    );
   }
 
   @override
   void navigateToSleepStatistics() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => StatisticsPage()));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => StatisticsPage()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-            'Sleep Data'
-        ),
+        title: const Text('Sleep Data'),
         backgroundColor: Colors.deepPurpleAccent,
         toolbarHeight: 100,
       ),
@@ -81,6 +92,16 @@ class _SleepDataState extends State<SleepData> implements SleepDataContractView 
               MenuButton(
                 text: 'Sleep Statistics',
                 onPressed: () => _presenter.onSleepStatisticsPressed(),
+              ),
+              const SizedBox(height: 10),
+              MenuButton(
+                text: 'Screen Time',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ScreenTimePage()),
+                  );
+                },
               ),
             ],
           ),
