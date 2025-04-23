b36c4088605cc070e3c08f97bf6c25932993876f
chock023 <chock023@d.umn.edu>
2025-04-23 18:36:14 -0500

    updated firebase configuration so playlists save to user account

--- lib/dreams/repositories/assessment_repository.dart
+++ lib/dreams/repositories/assessment_repository.dart
@@ -5,24 +5,47 @@ import '../models/goal.dart';
 
 class AssessmentRepository {
   final _firestore = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
+  final _auth      = FirebaseAuth.instance;
+
   String get _uid => _auth.currentUser!.uid;
 
+  //save one assessment run
   Future<void> saveAssessment({
     required List<AssessmentResponse> responses,
     required List<Goal> goals,
   }) {
-    final doc = _firestore
+    return _firestore
         .collection('users')
         .doc(_uid)
         .collection('assessments')
-        .doc(); //auto ID
-    return doc.set({
+        .add({
       'timestamp': FieldValue.serverTimestamp(),
-      'responses': responses
-          .map((r) => {'q': r.questionId, 'a': r.answer.toString()})
-          .toList(),
-      'goals': goals.map((g) => {'q': g.questionId, 's': g.suggestion}).toList(),
+      'responses': responses.map((r) => {
+        'q': r.questionId,
+        'a': r.answer.toString(),
+      }).toList(),
+      'goals': goals.map((g) => {
+        'q': g.questionId,
+        's': g.suggestion,
+      }).toList(),
+    });
+  }
+
+  Stream<List<Map<String, dynamic>>> watchAssessments() {
+    return _auth.authStateChanges().asyncExpand((user) {
+      if (user == null) return Stream.value(<Map<String, dynamic>>[]);
+      return _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('assessments')
+          .orderBy('timestamp', descending: true)
+          .snapshots()
+          .map((snap) => snap.docs.map((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      }).toList());
     });
   }
 }
+
--- lib/dreams/repositories/playlist_repository.dart
+++ lib/dreams/repositories/playlist_repository.dart
@@ -1,40 +1,55 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../models/playlist.dart';
 
 class PlaylistRepository {
   final _firestore = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
+  final _auth      = FirebaseAuth.instance;
 
   String get _uid => _auth.currentUser!.uid;
 
   Stream<List<Playlist>> watchPlaylists() {
-    return _firestore
-        .collection('users/$_uid/playlists')
-        .snapshots()
-        .map((snap) => snap.docs
-        .map((doc) => Playlist.fromMap(doc.data(), doc.id))
-        .toList());
+    return _auth.authStateChanges().asyncExpand((user) {
+      if (user == null) return Stream.value(<Playlist>[]);
+      return _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('playlists')
+          .snapshots()
+          .map((snap) => snap.docs
+          .map((doc) => Playlist.fromMap(doc.data(), doc.id))
+          .toList());
+    });
   }
 
   Future<void> createPlaylist(String name) {
     return _firestore
-        .collection('users/$_uid/playlists')
+        .collection('users')
+        .doc(_uid)
+        .collection('playlists')
         .add({'name': name, 'videoUrls': []});
   }
 
   Future<void> addVideoToPlaylist(String playlistId, String videoUrl) {
-    final ref = _firestore.doc('users/$_uid/playlists/$playlistId');
-    return ref.update({
-      'videoUrls': FieldValue.arrayUnion([videoUrl])
+    return _firestore
+        .collection('users')
+        .doc(_uid)
+        .collection('playlists')
+        .doc(playlistId)
+        .update({
+      'videoUrls': FieldValue.arrayUnion([videoUrl]),
     });
   }
 
   Future<void> removeVideoFromPlaylist(String playlistId, String videoUrl) {
-    final ref = _firestore.doc('users/$_uid/playlists/$playlistId');
-    return ref.update({
-      'videoUrls': FieldValue.arrayRemove([videoUrl])
+    return _firestore
+        .collection('users')
+        .doc(_uid)
+        .collection('playlists')
+        .doc(playlistId)
+        .update({
+      'videoUrls': FieldValue.arrayRemove([videoUrl]),
     });
   }
 }
+
